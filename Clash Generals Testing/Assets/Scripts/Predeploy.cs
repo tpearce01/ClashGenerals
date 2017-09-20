8a07b4597e1815bed1e4a20280df844039924dc1
Tyler Pearce <pearcetyler@yahoo.com>
2017-09-20 15:24:45 -0700

    Implemented "Predeploy"
    - Unit icon properly displayed and layered after selection
    - Constrained deployment location to bottom area
    - Increased center building health

---
+++ Clash Generals Testing/Assets/Scripts/Predeploy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Predeploy : MonoBehaviour, IPointerDownHandler {
+    public bool mouseDown = false;
+    public GameObject targetPrefab;
+    public GameObject unitToSpawnPrefab;
+    public GameObject targetObj;
+    public float cost;
+
+    const float MIN_Z_DEPLOY = -6.5f;
+    const float MAX_Z_DEPLOY = -2.5f;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (ResourceController.master.GetValue() > cost) {
+            targetObj = Instantiate(targetPrefab, GetMousePos(), Quaternion.identity);
+            targetObj.transform.parent = gameObject.transform.parent.transform.parent.transform;
+            mouseDown = true;
+        }
+    }
+
+    Vector3 GetMousePos() {
+        return Input.mousePosition;
+    }
+
+    Vector3 GetMousePosWorld() {
+        Vector3 pos = Input.mousePosition;
+        pos.z = 30;
+        return Camera.main.ScreenToWorldPoint(pos);
+    }
+
+    void Update() {
+        if (mouseDown) {
+            if (Input.GetKeyUp(KeyCode.Mouse0)) {
+                Vector3 deployPosition = GetMousePosWorld();
+                mouseDown = false;
+                Destroy(targetObj);
+
+                //If the deployment position is in a valid location, spawn the unit
+                if (deployPosition.z > MIN_Z_DEPLOY && deployPosition.z < MAX_Z_DEPLOY) {
+                    GameObject temp = Instantiate(unitToSpawnPrefab, GetMousePosWorld(), Quaternion.identity);
+                    ResourceController.master.Modify(-cost);
+                }
+            }
+            else {
+                targetObj.transform.position = GetMousePos();
+            }
+        }
+    }
+}
